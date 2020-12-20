39db39a609d9497c4dbf5e43c735146be290c99c
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2020-12-20 15:09:59 +0100

    Add Register Controller

---
+++ Web/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+
+        public IActionResult RegisterPirate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
