76972182e80273076a855d653a164c00ff911e7c
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2020-12-20 12:54:47 +0100

    Change folder

--- GalacticSpaceTransitAuthority/Engine.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GalacticSpaceTransitAuthority
-{
-    public class Engine
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Energy { get; set; }
-    }
-}
---
+++ Ship.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GalacticSpaceTransitAuthority
+{
+    public class Ship
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Hull Hull { get; set; }
+        public List<Wing> Wings { get; set; }
+        public Engine Engine { get; set; }
+        public int Energy => Engine?.Energy + Wings?.Sum(w => w.Energy) ?? 0;
+    }
+}
