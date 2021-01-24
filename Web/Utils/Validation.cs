f81e917f57d9ebe5ccaca55b9b2bcd728aac6051
Lars Meeuwsen <larsmeeuwsen@gmail.com>
2021-01-24 17:23:22 +0100

    Add ValidateKineticWeapons

--- Web/Utils/Validation.cs
+++ Web/Utils/Validation.cs
@@ -10,7 +10,7 @@ namespace Web.Utils
         private static ModelStateDictionary _modelState;
         private static Ship _ship;
         private static IEnumerable<Weapon> _weapons;
-        
+
         public static void ValidateShip(ModelStateDictionary modelState, Ship ship)
         {
             _modelState = modelState;
@@ -22,8 +22,9 @@ namespace Web.Utils
             ValidateImploderWeapons();
             ValidateCombinationWeapons();
             ValidateNullifierWeapon();
+            ValidateKineticWeapons();
         }
-        
+
         private static void ValidateNumberOfWeapons()
         {
             foreach (var wing in _ship.Wings.Where(wing => wing.Hardpoint.Count > wing.NumberOfHardpoints))
@@ -39,27 +40,44 @@ namespace Web.Utils
         private static void ValidateEnergyConsumption()
         {
             if (Calculations.GetEnergyConsumption(_weapons) > _ship.Energy)
-                _modelState.AddModelError("EnergyConsumptionOverdraft", "The energy consumption of the ship is too high");
+                _modelState.AddModelError("EnergyConsumptionOverdraft",
+                    "The energy consumption of the ship is too high");
         }
 
         private static void ValidateImploderWeapons()
         {
             if (_ship.Engine.Id == 2 && _weapons.Any(weapon => weapon.Id == 9))
-                _modelState.AddModelError("ImplosionDanger", "The combination of Imploder weapon and Intrepid Class engine is not allowed");
+                _modelState.AddModelError("ImplosionDanger",
+                    "The combination of Imploder weapon and Intrepid Class engine is not allowed");
         }
 
         private static void ValidateCombinationWeapons()
         {
-            if (_weapons.Any(weapon => weapon.DamageType == DamageTypeEnum.Heat) && _weapons.Any(weapon => weapon.DamageType == DamageTypeEnum.Cold))
+            if (_weapons.Any(weapon => weapon.DamageType == DamageTypeEnum.Heat) &&
+                _weapons.Any(weapon => weapon.DamageType == DamageTypeEnum.Cold))
                 _modelState.AddModelError("HeatStress", "The combination of heat and cold weapons is not allowed");
-            if (_weapons.Any(weapon => weapon.DamageType == DamageTypeEnum.Statis) && _weapons.Any(weapon => weapon.DamageType == DamageTypeEnum.Gravity))
-                _modelState.AddModelError("ForceStress", "The combination of statis and gravity weapons is not allowed");
+            if (_weapons.Any(weapon => weapon.DamageType == DamageTypeEnum.Statis) &&
+                _weapons.Any(weapon => weapon.DamageType == DamageTypeEnum.Gravity))
+                _modelState.AddModelError("ForceStress",
+                    "The combination of statis and gravity weapons is not allowed");
         }
 
         private static void ValidateNullifierWeapon()
         {
-            foreach (var wing in _ship.Wings.Where(wing => wing.Hardpoint.Any(weapon => weapon.Id == 14) && wing.Hardpoint.Count < 2))
+            foreach (var wing in _ship.Wings.Where(wing =>
+                wing.Hardpoint.Any(weapon => weapon.Id == 14) && wing.Hardpoint.Count < 2))
                 _modelState.AddModelError("LoneNullifier", "The Nullifier can't be the only weapon on " + wing.Name);
+        }
+
+        private static void ValidateKineticWeapons()
+        {
+            var energyDrainWing = _ship.Wings.Where(wing =>
+                    wing.Hardpoint.Any(weapon => weapon.DamageType == DamageTypeEnum.Kinetic))
+                .Select(wing => wing.Hardpoint.Where(weapon => weapon.DamageType == DamageTypeEnum.Kinetic)
+                    .Sum(weapon => weapon.EnergyDrain)).ToList();
+
+            if (energyDrainWing.Max() - energyDrainWing.Min() > 35)
+                _modelState.AddModelError("KineticDifference", "The energy drain of kinetic weapons on different wings can't be more than 35");
         }
     }
 }
