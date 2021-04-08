d42a2589e07f295a1ed08a3d9ccfc16fe442a3b4
Maik de Kruif <maik@maik.dev>
2021-04-08 19:17:25 +0200

    Separate controller tests from validation

---
+++ xUnitTest/ShipControllerTest.cs
@@ -0,0 +1,38 @@
+using Data.Service;
+using Microsoft.AspNetCore.Mvc;
+using Web.Controllers;
+using Web.Utils;
+using Xunit;
+
+namespace xUnitTest
+{
+    public class ShipControllerTest
+    {
+        private readonly RegisterShipController _shipController;
+
+        public ShipControllerTest()
+        {
+            var spaceTransitAuthority = new SpaceTransitAuthority();
+            var calculations = new Calculations();
+            _shipController = new RegisterShipController(spaceTransitAuthority, calculations);
+        }
+        
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        public void CheckEvenNumberOfWingsFrontend(int amount)
+        {
+            var jsonResult = _shipController.VerifyNumberOfWings(amount) as JsonResult;
+            
+            if (amount % 2 == 0)
+                Assert.Equal("True", jsonResult!.Value.ToString());
+            else
+                Assert.NotEqual("True", jsonResult!.Value.ToString());
+        }
+    }
+}
--- xUnitTest/ValidationTest.cs
+++ xUnitTest/ValidationTest.cs
@@ -1,42 +1,13 @@
-using Data.Service;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Moq;
-using Web.Controllers;
 using Web.Utils;
 using Xunit;
-using Xunit.Abstractions;
 using xUnitTest.Factories;
 
 namespace xUnitTest
 {
     public class ValidShipTest
     {
-        private readonly ITestOutputHelper _testOutputHelper;
-        private readonly RegisterShipController _shipController;
-
-        public ValidShipTest(ITestOutputHelper testOutputHelper)
-        {
-            _testOutputHelper = testOutputHelper;
-            var spaceTransitAuthority = new SpaceTransitAuthority();
-            var calculations = new Calculations();
-            _shipController = new RegisterShipController(spaceTransitAuthority, calculations);
-        }
-
-        [Fact]
-        public void CheckEvenNumberOfWingsFrontend()
-        {
-            for (var i = 1; i < 10; i++)
-            {
-                var jsonResult = _shipController.VerifyNumberOfWings(i) as JsonResult;
-                
-                if (i % 2 == 0)
-                    Assert.Equal("True", jsonResult!.Value.ToString());
-                else
-                    Assert.NotEqual("True", jsonResult!.Value.ToString());
-            }
-        }
-        
         [Theory]
         [InlineData(-1)]
         [InlineData(0)]
